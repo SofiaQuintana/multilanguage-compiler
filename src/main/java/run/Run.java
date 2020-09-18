10228f7f70b38899ee476927efdaf91674c25fa4
SofiaQuintana <sofiaquintana2@gmail.com>
2020-09-17 23:34:11 -0600

    Added: project pom config and run

---
+++ src/main/java/run/Run.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package run;
+import graphical_user_interface.PrincipalPanel;
+/**
+ *
+ * @author zofia
+ */
+public class Run {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        PrincipalPanel panel = new PrincipalPanel();
+        panel.setVisible(true);
+    }
+    
+}
