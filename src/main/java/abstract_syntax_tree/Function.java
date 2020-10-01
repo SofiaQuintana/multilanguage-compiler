9161a3b5e9fe7bafe4546519b00969a4f4ecfada
SofiaQuintana <sofiaquintana2@gmail.com>
2020-09-30 18:34:04 -0600

    Added: function ast object (empty)

---
+++ src/main/java/abstract_syntax_tree/Function.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package abstract_syntax_tree;
+
+import abstract_syntax_tree.environment.Environment;
+
+/**
+ *
+ * @author zofia
+ */
+public class Function implements Instruction {
+    @Override
+    public Object execute(Environment environment) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    @Override
+    public int getLine() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getColumn() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
